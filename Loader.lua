cfedbf38b3685f7b9c41d55e5983be348370a083
EwItsCaydon <caydonnahm@gmail.com>
2024-12-22 23:50:42 -0500

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,2 +1,2 @@
 script_key = _G.Project_Saturn_Key;
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/1dfbb37e56ca3c7a3c17a496558b9721.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/036b65680886b059da17cfe707c8d36e.lua"))()
